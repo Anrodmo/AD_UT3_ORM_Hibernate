9cf8c0ccd76ab2166e149dcd1a0e84b70a550f4a
Anrodmo <131297046+Anrodmo@users.noreply.github.com>
2024-01-15 20:12:20 +0100

    Práctica terminada
    Todos los métodos creados y funcionando.
    Fin de la práctica

--- src/logica/HibernateEnterprise.java
+++ src/logica/HibernateEnterprise.java
@@ -1,9 +1,17 @@
 package logica;
 
+
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.ObjectNotFoundException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+//import org.hibernate.mapping.List;
 
 import miEmpresa.*;
 
@@ -41,6 +49,120 @@ public class HibernateEnterprise {
 			session.close();   // Cerramos la sesión
 		}
 	}
+	
+	public void cerrarSesionFactory() {
+		sf.close();
+	}
+	
+	public void mostrarProductos(){
+		Session sesion = sf.openSession();  // abro sesión del SesionFactory
+		Transaction tx = null;              // abro una conexión??
+		
+		try {	
+	         tx = sesion.beginTransaction();
+	         List allproducts = sesion.createQuery("FROM Productos").list(); 
 
+	         Iterator it =  allproducts.iterator();
+	         while(it.hasNext()){
+	        // for (Iterator iterator = allproducts.iterator(); iterator.hasNext();){
+	            Productos p = (Productos) it.next(); 
+	            System.out.print("Id: " + p.getId()); 
+	            System.out.print("  ,Name: " + p.getNombre()); 
+	            System.out.println("  ,Price: " + p.getPrecio()); 
+	         }
+	         tx.commit();
+	      } catch (HibernateException e) {
+	         if (tx!=null) tx.rollback();
+	         e.printStackTrace(); 
+	      } finally {
+	    	  sesion.close(); 
+	      }
+
+	}
+	
+	public Productos encontrarProductoPorId(int id) {
+		Session session = sf.openSession();
+		Transaction tx = null;
+		Productos p=new Productos();
+
+		try{
+			System.out.println("loading the object from the database");
+			tx=session.beginTransaction();
+			p=(Productos)session.load(Productos.class, id);
+			tx.commit();
+			System.out.println("The product with id="+id+" is:"+p.getNombre());
+			
+			}catch(ObjectNotFoundException e){
+				if(tx!=null){
+					System.out.println(e);
+					System.out.println("Product not found");			
+				}
+			}catch(Exception e){
+				if(tx!=null){
+					System.out.println(e);
+					tx.rollback();
+				}
+			}finally{
+				session.close();
+			}
+		
+		return p;
+	}
+	
+	
+	public void borrarProductoPorId(int id) {
+		Productos p = new Productos();
+		Session session=sf.openSession();
+		Transaction tx=null;
+		
+		try{
+			tx=session.beginTransaction();
+			p=(Productos)session.load(Productos.class, id);
+			session.delete(p);
+			tx.commit();
+			System.out.printf("Object deleted FROM THE DATABASE: %s, %s, %s \n",p.getId(),p.getNombre(),p.getPrecio());
+		}catch(Exception e){
+			if(tx!=null){
+				tx.rollback();
+			}
+		}finally{
+			session.close();
+		}
+		
+	}
+	
+	public void editarProductoPorId(int id, String nuevoNombre, double nuevoPrecio) {
+		Productos p = new Productos();
+		Session session=sf.openSession();
+		Transaction tx=null;
+		
+		try{
+			tx=session.beginTransaction();
+			System.out.println("Updating a value");
+			System.out.println("Before updating, we need to load the object");
+			p=(Productos)session.load(Productos.class, id);//we load the pen drive
+			p.setPrecio(nuevoPrecio);//we change the properties 
+			p.setNombre(nuevoNombre);
+			session.update(p);//we update the values in the database
+			tx.commit();
+			System.out.println("Object updated");
+			
+		// añadimos esta excepción para poder tratar el caso	
+		}catch(ObjectNotFoundException ex) {
+			// podemos hacer que si no existe lo cree
+			//this.addProduct(id, nuevoNombre, nuevoPrecio);
+			// o solo que nos avise de que no existe producto con ese id
+			System.out.println("No existe un Producto con el id: "+id);
+		}catch(Exception e){
+			System.out.println(e);
+			if(tx!=null){
+				tx.rollback();
+			}
+		}finally{
+			session.close();
+		}	
+		
+	}
+	
 	
 }
--- src/logica/UsarHibernate.java
+++ src/logica/UsarHibernate.java
@@ -1,14 +1,39 @@
 package logica;
 
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+
 public class UsarHibernate {
 
 	public static void main(String[] args) {
+		/* Para eliminar los avisos de INFO de log */
+		LogManager.getLogManager().getLogger("").setLevel(Level.SEVERE);
 		
+		/* Creamos la SesionFactory de la conexión*/
 		HibernateEnterprise conexionHibernate = new HibernateEnterprise();
 		
-		conexionHibernate.addProduct(3, "monitor", 170);
 		
 		
+		/* Este método abre y cierra una sesión en la sesSesionFactoryión y hace el insert*/
+		//conexionHibernate.addProduct(3, "Torre AMD", 55);
+		
+		/* Mostramos todos los productos de la BBDD*/
+		conexionHibernate.mostrarProductos();
+		
+		/* Buscamos un producto por id*/
+		conexionHibernate.encontrarProductoPorId(9);
+		
+		System.out.println("==================================================\n\n");
+		
+		/* Vamos a borrar por id, el 9 en este caso */
+		
+		//conexionHibernate.borrarProductoPorId(4);
+		
+		/* Vamos a modificar el id 5*/
+		conexionHibernate.editarProductoPorId(55, "Radeon 5000", 465.99);
+		
+		/* Cerramos la SesionFactory*/ 
+		conexionHibernate.cerrarSesionFactory();
 
 	}
 
